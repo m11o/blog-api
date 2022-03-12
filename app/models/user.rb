04d9dc987aa908cc6f827dde1c9c26dac573826a
Masahito Osako <velonica.gale2227@gmail.com>
2022-03-12 18:34:02 +0900

    add user model and test

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  email            :string(255)      not null
+#  crypted_password :string(255)
+#  salt             :string(255)
+#  name             :string(255)      default(""), not null
+#  hostname         :string(255)      default(""), not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+end
